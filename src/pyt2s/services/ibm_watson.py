b5e9459137a28e3b566687da4ed9c6a550a04743
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2024-04-02 03:54:54 +0530

    fix: ibm watson

--- src/pyt2s/services/ibm_watson.py
+++ src/pyt2s/services/ibm_watson.py
@@ -10,16 +10,22 @@ class IBM_Watson(Service):
         'fr-FR_ReneeV3Voice', 'fr-CA_LouiseV3Voice', 'de-DE_BirgitV3Voice', 'de-DE_DieterV3Voice', 'de-DE_ErikaV3Voice', 'it-IT_FrancescaV3Voice', 
         'ja-JP_EmiV3Voice', 'ko-KR_JinV3Voice', 'pt-BR_IsabelaV3Voice', 'es-ES_EnriqueV3Voice', 'es-ES_LauraV3Voice', 'es-LA_SofiaV3Voice', 'es-US_SofiaV3Voice' ]
 
-    __session__ = requests.session()   
+    __session__ = requests.session()
     __url1__ = 'https://www.ibm.com/demos/live/tts-demo/api/tts/session'   
     __url2__ = 'https://www.ibm.com/demos/live/tts-demo/api/tts/store'   
     __url3__ = 'https://www.ibm.com/demos/live/tts-demo/api/tts/newSynthesizer'
 
+    __headers__ = {
+        'Origin': 'https://www.ibm.com',
+        'Referer': 'https://www.ibm.com/demos/live/tts-demo/self-service/home',
+        'Accept': 'application/json, text/plain, */*',
+    }
+
     def requestTTS(self, text: str, voice = 'en-GB_CharlotteV3Voice'):
         super().requestTTS(text, voice)
-        id = str(uuid.uuid4())       
-        self.__session__.post(self.__url1__)       
-        res = self.__session__.get(self.__url2__, params=params)       
-        params = {'ssmlText': f'<prosody pitch=\'0%\' rate=\'-0%\'>{text}</prosody>', 'sessionID': id}       
-        res = self.__session__.get(f'{self.__url3__}?voice={voice}&id={id}')
+        self.__session__.post(self.__url1__, headers=self.__headers__)
+        id = str(uuid.uuid4())    
+        jsonPayload = {"ssmlText": f"<prosody pitch=\"0%\" rate=\"-0%\">{text}</prosody>", "sessionID": id}   
+        self.__session__.post(self.__url2__, data=jsonPayload, headers=self.__headers__)
+        res = self.__session__.get(self.__url3__, params={'voice' : voice,'id': id})
         return res.content
