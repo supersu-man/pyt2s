a80925dd90f2004296fe8c9094b0372c0bd20d87
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2024-04-14 04:56:55 +0530

    fixed 404 error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
